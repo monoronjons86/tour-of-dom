966beb2111b644a7d916be693d029c890ddfc541
monoronjon sarker <monoronjons86@gmail.com>
2023-02-10 00:21:12 +0600

    even

--- js-dom-event/event.js
+++ js-dom-event/event.js
@@ -1,1 +1,20 @@
 console.log('this is separate js file')
+
+//option 2:add onclick function
+function makeRed(){
+    document.body.style.backgroundColor='red';
+}
+
+
+//option 3:
+    const makeBlueButton=document.getElementById('make-blue');
+    makeBlueButton.onclick =makeBlue;
+    function makeBlue(){
+        document.body.style.backgroundColor='blue'
+    }
+
+//option 2:another
+const purpleButton=document.getElementById('make-purple')
+purpleButton.onclick=function makePurple(){
+    document.body.style.backgroundColor='purple'
+}
