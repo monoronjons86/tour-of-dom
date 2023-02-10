24bf63f47534c6331c5fbeb07a3caed54b8f79ca
monoronjon sarker <monoronjons86@gmail.com>
2023-02-10 16:53:54 +0600

    today practice

---
+++ js-dom-event/practice.js
@@ -0,0 +1,10 @@
+function greenMe(){
+    document.body.style.backgroundColor='green';
+}
+
+const makeBlueButton=document.getElementById('make-blue')
+makeBlueButton.onclick=makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor='blue';
+}
