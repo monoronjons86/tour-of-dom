383978894416c0133596546cadcc2a0bbe23e18e
monoronjon sarker <monoronjons86@gmail.com>
2023-02-10 19:12:36 +0600

    enven-learn

--- js-dom-event/event.js
+++ js-dom-event/event.js
@@ -13,8 +13,28 @@ function makeRed(){
         document.body.style.backgroundColor='blue'
     }
 
-//option 2:another
+//option 3:another
 const purpleButton=document.getElementById('make-purple')
 purpleButton.onclick=function makePurple(){
     document.body.style.backgroundColor='purple'
 }
+
+//option 4
+
+    const makePinkButton=document.getElementById('make-pink');
+    makePinkButton.addEventListener('click',makePink)
+
+    function makePink(){
+    document.body.style.backgroundColor='pink';
+    }
+
+//another 4:another
+    const makeGreenButton=document.getElementById('make-green');
+    makeGreenButton.addEventListener('click', function makeGreen(){
+        document.body.style.backgroundColor='green';
+    })
+
+//option 04:final
+    document.getElementById('make-goldenred').addEventListener('click',function(){
+        document.body.style.backgroundColor='goldenrod'
+    })
