6377c4201fb07d2a50a16b6b77d3fff1ce5c690f
monoronjon sarker <monoronjons86@gmail.com>
2023-02-13 21:50:54 +0600

    sontanadd

--- Bap-ar-bank/js/login.js
+++ Bap-ar-bank/js/login.js
@@ -13,7 +13,7 @@ const password=passwordFiled.value;
 
 //Danger:Do not verify email password on the client side
 //step-4:verify email and password
-if(email==='sontan@baap.com'&& password=='secret'){
+if(email==='sontan@bap.com'&& password=='secret'){
     window.location.href='bank.html'
 }
 else{
