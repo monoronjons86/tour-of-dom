739cc6599fff869bacfb16cad988c6168eaa0234
monoronjon sarker <monoronjons86@gmail.com>
2023-02-18 10:39:59 +0600

    deposit-js

--- again-bap-bank/js/deposit.js
+++ again-bap-bank/js/deposit.js
@@ -1,8 +1,26 @@
 document.getElementById('btn-deposit').addEventListener('click',function(){
   const depositFiled=document.getElementById('user-deposit');
-  const depositAmount=depositFiled.value ;
+  const newDepositAmountString=depositFiled.value ;
+  const newDepositAmount=parseFloat(newDepositAmountString);
 
   const depositTotalElement=document.getElementById('deposit-total');
-  const depositTotal=depositTotalElement.innerText;
-  depositTotalElement.innerText=depositAmount;
+  const previousDepositTotalString=depositTotalElement.innerText;
+  const previousDepositTotal=parseFloat(previousDepositTotalString);
+
+  const currentDepositTotal=newDepositAmount+previousDepositTotal;
+  depositTotalElement.innerText=currentDepositTotal;
+
+  const balanceTotalElement=document.getElementById('balance-total');
+  const previousBalanceTotalString=balanceTotalElement.innerText;
+  const previousBalanceTotal=parseFloat(previousBalanceTotalString);
+
+  const currentBalanceTotal=previousBalanceTotal+newDepositAmount;
+  balanceTotalElement.innerText=currentBalanceTotal;
+
+
+
+  depositFiled.value='';
+
+
+
 });
