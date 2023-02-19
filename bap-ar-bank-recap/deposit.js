98d733cabe3b0ec6e8501dea6e7b866a28d1362b
monoronjon sarker <monoronjons86@gmail.com>
2023-02-19 19:31:28 +0600

    deposit-added

---
+++ bap-ar-bank-recap/deposit.js
@@ -0,0 +1,16 @@
+document.getElementById('btn-deposit').addEventListener('click',function(){
+ const depositFiled=document.getElementById('deposit-filed');
+ const newDepositAmountString=depositFiled.value ;
+ const newDepositAmount=parseFloat(newDepositAmountString);
+
+ depositFiled.value='';
+
+ const depositTotalElement=document.getElementById('deposit-total');
+ const previousDepositTotalString=depositTotalElement.innerText;
+ const previousDepositTotal=parseFloat(previousDepositTotalString);
+
+ const newDepositTotal=previousDepositTotal+newDepositAmount;
+ depositTotalElement.innerText=newDepositTotal;
+ 
+ 
+});
