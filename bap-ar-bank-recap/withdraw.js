da7f346e20864f9e65305b978df0abe8f8b13b3c
monoronjon sarker <monoronjons86@gmail.com>
2023-02-20 10:02:34 +0600

    withdraw added

--- bap-ar-bank-recap/withdraw.js
+++ bap-ar-bank-recap/withdraw.js
@@ -1,3 +1,25 @@
 document.getElementById('btn-withdraw').addEventListener('click',function(){
-console.log('withdraw click here');
+    const withdrawFiled=document.getElementById('withdraw-filed');
+    const newWithdrawAmountString=withdrawFiled.value ;
+    const newWithdrawAmount=parseFloat(newWithdrawAmountString);
+
+
+    withdrawFiled.value='';
+
+    const withdrawTotalElement=document.getElementById('withdraw-total');
+    const previousWithdrawTotalString=withdrawTotalElement.innerText;
+    const previousWithdrawTotal=parseFloat(previousWithdrawTotalString);
+
+    const newWithdrawTotal=previousWithdrawTotal+newWithdrawAmount;
+    withdrawTotalElement.innerText=newWithdrawTotal;
+
+    const balanceElement=document.getElementById('balance-total');
+    const previousBalanceTotalString=balanceElement.innerText;
+    const previousBalanceTotal=parseFloat(previousBalanceTotalString);
+
+    const newBalanceTotal=previousBalanceTotal-newWithdrawAmount;
+     balanceElement.innerText=newBalanceTotal;
+
+
+
 });
