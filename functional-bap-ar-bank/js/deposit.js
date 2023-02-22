e35bfcedcb31ed7af126fc4b8f32b6e5562c2b21
monoronjon sarker <monoronjons86@gmail.com>
2023-02-22 15:12:15 +0600

    element

--- functional-bap-ar-bank/js/deposit.js
+++ functional-bap-ar-bank/js/deposit.js
@@ -1,3 +1,19 @@
-document.getElementById('btn-withdraw').addEventListener('click',function(){
-    console.log('deposit button clicked');
+function getInputFiledValueById(inputFieldId){
+    const inputField=document.getElementById(inputFieldId);
+    const inputFiledValueString=inputField.value ;
+    const inputFiledValue=parseFloat(inputFiledValueString);
+    inputField.value=' ';
+    return inputFiledValue;
+}
+function getElementValueById(elementId){
+    const element=document.getElementById(elementId);
+    const elementValueString=element.innerText;
+    const value=parseFloat(elementValueString);
+    return value;
+}
+
+document.getElementById('btn-deposit').addEventListener('click',function(){
+    const newDepositAmount=getInputFiledValueById('deposit-filed')
+
+   
 });
