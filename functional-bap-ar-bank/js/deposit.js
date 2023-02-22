cb344c8543862218c072ca1f43edc8705c986cdc
monoronjon sarker <monoronjons86@gmail.com>
2023-02-22 16:41:50 +0600

    na pari

--- functional-bap-ar-bank/js/deposit.js
+++ functional-bap-ar-bank/js/deposit.js
@@ -5,15 +5,28 @@ function getInputFiledValueById(inputFieldId){
     inputField.value=' ';
     return inputFiledValue;
 }
-function getElementValueById(elementId){
-    const element=document.getElementById(elementId);
-    const elementValueString=element.innerText;
-    const value=parseFloat(elementValueString);
-    return value;
+// function getElementValueById(elementId){
+//     const element=document.getElementById(elementId);
+//     const elementValueString=element.innerText;
+//     const value=parseFloat(elementValueString);
+//     return value;
+// }
+function getTextElementValueById(elementId){
+    const textElement=document.getElementById(elementId);
+    const textElementValueString=textElement.innerText;
+    const textElementValue=parseFloat(textElementValueString);
+    return textElementValue;
+
+}
+function setTextElementValueById(elementId,newValue){
+    const textElement=document.getElementById(elementId);
+    textElement.innerText=newValue;
 }
 
 document.getElementById('btn-deposit').addEventListener('click',function(){
     const newDepositAmount=getInputFiledValueById('deposit-filed')
 
-   
+    const previousDepositTotal=getTextElementValueById('Deposit-total');
+    const newDepositTotal=previousDepositTotal+newDepositAmount;
+    setTextElementValueById('deposit-total',newDepositTotal)
 });
