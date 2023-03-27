e3b7d970c67f39e8c0b984485ee5016e519e63b6
monoronjon sarker <monoronjons86@gmail.com>
2023-03-27 21:25:44 +0600

    finish js book

--- book-list-dom/app.js
+++ book-list-dom/app.js
@@ -12,6 +12,20 @@ btn.addEventListener('click',function(e){
     else{
         const newRow=document.createElement('tr');
         //Creating new title
+        const newTitle=document.createElement('th');
+        newTitle.innerHTML=title.value;
+        newRow.appendChild(newTitle);
+
+        //Creating new title
+        const newAuthor=document.createElement('th');
+        newAuthor.innerHTML=author.value;
+        newRow.appendChild(newAuthor);
+
+        //Creating new title
+        const newYear=document.createElement('th');
+        newYear.innerHTML=year.value;
+        newRow.appendChild(newYear);
         
+        bookList.appendChild(newRow);
     }
 });
