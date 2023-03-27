6ae2e15c81bfacc99b1cefa681fffa9195cf22bd
monoronjon sarker <monoronjons86@gmail.com>
2023-03-27 11:33:50 +0600

    few js

--- book-list-dom/app.js
+++ book-list-dom/app.js
@@ -1,3 +1,16 @@
 const title= document.querySelector('#title');
 const author=document.querySelector('#author');
 const year=document.querySelector('#year');
+const btn=document.querySelector('.btn');
+const bookList=document.querySelector('#book-list');
+
+btn.addEventListener('click',function(e){
+    e.preventDefault();
+    if(title.value==''&& author.value==''&& year.value==''){
+        alert('Bhai kicu akta dan');
+    }
+    else{
+        const newRow=document.createElement('tr');
+        console.log(newRow);
+    }
+});
